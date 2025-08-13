a0d5841b862c90a35a3f417ecfeaa7f02f7bfdad
M.Hamed <mhamed@victorylink.com>
2025-08-13 17:30:29 +0300

    Feat: Add status bar padding and background color utility
    A new function `setupStatusBarPadding` has been added to `UiHelper.kt`.
    This function configures the top padding of a window's decor view to accommodate the status bar and sets the background color of the status bar area.
    
    - It uses `ViewCompat.setOnApplyWindowInsetsListener` to listen for window inset changes.
    - It retrieves the status bar insets using `windowInsets.getInsets(WindowInsetsCompat.Type.statusBars())`.
    - It updates the decor view's padding to include the status bar height at the top.
    - It sets the background color of the decor view to the provided color.

--- app/src/main/java/com/example/devpilotandroidnative/UiHelper.kt
+++ app/src/main/java/com/example/devpilotandroidnative/UiHelper.kt
@@ -1,18 +1,19 @@
 package com.example.devpilotandroidnative
 
-import android.R
 import android.app.Activity
 import android.app.StatusBarManager
 import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.drawable.ColorDrawable
+import android.view.Window
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.bumptech.glide.Glide
 
 
@@ -24,6 +25,16 @@ object UiHelper {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    fun setupStatusBarPadding(window: Window, color: Int) {
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) 
+        { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.statusBars())
+            v.updatePadding(0, insets.top, insets.right, 0)
+            v.setBackgroundColor(color)
+            WindowInsetsCompat.CONSUMED
         }
     }
 
