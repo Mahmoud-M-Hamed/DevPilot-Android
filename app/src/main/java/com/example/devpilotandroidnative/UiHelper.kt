88a006d126acf1dc0f098c390752f425cee835b3
M.Hamed <mhamed@victorylink.com>
2025-08-14 16:32:29 +0300

    Refactor: Remove unused `applyEdgeToEdge` function
    The `applyEdgeToEdge` function in `UiHelper.kt` has been removed as it is no longer used. The `enableEdgeToEdge()` function from `WindowCompat` is now called directly in `MainActivity`.

--- app/src/main/java/com/example/devpilotandroidnative/UiHelper.kt
+++ app/src/main/java/com/example/devpilotandroidnative/UiHelper.kt
@@ -1,16 +1,10 @@
 package com.example.devpilotandroidnative
 
-import android.app.Activity
-import android.app.StatusBarManager
 import android.content.Context
-import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.drawable.ColorDrawable
 import android.view.Window
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toolbar
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,15 +13,6 @@ import com.bumptech.glide.Glide
 
 
 object UiHelper {
-    
-    fun applyEdgeToEdge(activity: Activity, rootViewId: Int) {
-        ViewCompat.setOnApplyWindowInsetsListener(activity.findViewById(rootViewId))
-        { view, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
 
      fun setupStatusBarPadding(window: Window, color: Int) {
         ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, windowInsets ->
