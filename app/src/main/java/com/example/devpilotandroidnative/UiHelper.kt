ec6ccef90d62d793f4d977e1f40decc757527c28
M.Hamed <mhamed@victorylink.com>
2025-08-14 16:12:53 +0300

    Refactor: Adjust status bar padding and appearance in `UiHelper`
    - Modified `setupStatusBarPadding` to apply system bar insets for padding on all sides (left, top, right, bottom) instead of just top and right.
    - Set `isAppearanceLightStatusBars` to `false` within `setupStatusBarPadding` to ensure dark icons/text on the status bar, suitable for a light-colored status bar background.
    - The background color setting for the decor view remains.

--- app/src/main/java/com/example/devpilotandroidnative/UiHelper.kt
+++ app/src/main/java/com/example/devpilotandroidnative/UiHelper.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import android.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import com.bumptech.glide.Glide
@@ -28,15 +29,25 @@ object UiHelper {
         }
     }
 
-    fun setupStatusBarPadding(window: Window, color: Int) {
-        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) 
-        { v, windowInsets ->
+     fun setupStatusBarPadding(window: Window, color: Int) {
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, windowInsets ->
             val insets = windowInsets.getInsets(WindowInsetsCompat.Type.statusBars())
-            v.updatePadding(0, insets.top, insets.right, 0)
+            v.updatePadding(insets.left, insets.top, insets.right, insets.bottom)
+            WindowCompat.getInsetsController(window, window.decorView).apply {
+                isAppearanceLightStatusBars = false
+            }
+
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+
             v.setBackgroundColor(color)
             WindowInsetsCompat.CONSUMED
+
         }
+
     }
+    
 
     fun retrieveImage(
         context: Context,
